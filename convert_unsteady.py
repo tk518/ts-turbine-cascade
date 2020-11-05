1b732f5910f61975bbf97b33874133401c0b69ca
Thomas K <tk518@cam.ac.uk>
2020-11-05 13:17:56 +0000

    Probe added

--- convert_unsteady.py
+++ convert_unsteady.py
@@ -13,13 +13,17 @@ if __name__ == "__main__":
         # Time steps per cycle - leave at 72
         nstep_cycle = 72
 
+        # Which time step to start saving probes
+        nstep_save_start =  (ncycle-3)*nstep_cycle
+
+        # Number of time steps between probes
+        nstep_save_probe = 9
+
         # File name of the steady soln to read in
         fname = "output_"+str(x)+".hdf5"
 
         # File name of the new unsteady input file to write out
         fname_out = "input_2_"+str(x)".hdf5"
-
-
 
         #
         # Should not need to change below this line
@@ -138,11 +142,21 @@ if __name__ == "__main__":
             p.pid = g2.add_patch(1, p)
             g2.set_pv('probe_append', ts_tstream_type.int, p.bid, p.pid, 1)
 
-        # load balance for 1 GPUs
-        ts_tstream_load_balance.load_balance(g2, 1)
+            # variables for unsteady run
+            g2.set_av("ncycle", ts_tstream_type.int, ncycle)
+            g2.set_av("frequency", ts_tstream_type.float,  freq)
+            g2.set_av("nstep_cycle", ts_tstream_type.int, nstep_cycle)
+            g2.set_av("nstep_inner", ts_tstream_type.int, 200)
+            
+            # disable saving of snapshots
+            g2.set_av("nstep_save", ts_tstream_type.int, 999999)
 
-        # Reset spurious application variable
-        g2.set_av("if_ale", ts_tstream_type.int, 0)
+            # Save probes for last few period
+            g2.set_av("nstep_save_start", ts_tstream_type.int, nstep_save_start)
+            g2.set_av("nstep_save_start_probe", ts_tstream_type.int, nstep_save_start)
 
-        # write out unsteady input file
-        g2.write_hdf5(fname_out)
+            # save probes every  nth step, from the beginning
+            g2.set_av("nstep_save_probe", ts_tstream_type.int, nstep_save_probe)
+
+            # other configuration variables
+            g2.set_av("dts_conv", ts_tstream_type.float, 0.0005)
