5a293eaa8cfb4f2672b70ddb658bf2ff597c8999
Thomas K <tk518@cam.ac.uk>
2021-01-25 18:41:21 +0000

    Future proofing flow and stage loading coeff

--- Source/convert_unsteady.py
+++ Source/convert_unsteady.py
@@ -7,177 +7,181 @@ from ts import ts_tstream_load_balance, ts_tstream_patch_kind
 fnamein = "output_1"
 
 if __name__ == "__main__":
-    for Mai in [0.65, 0.7, 0.75, 0.81]:
-        # Number of rotor blade passing periods to run for
-        # Change me so that the computaion reaches a periodic state
-        ncycle = 70
+    for Psii in [1.6]:
 
-        # Time steps per cycle 
-        # Increased a bit because usually have fewer rotor blades
-        nstep_cycle = 96
+        for Phii in [0.4, 0.6, 0.8, 1.0, 1.2]:
 
-        # Which time step to start saving probes
-        nstep_save_start =  (ncycle-5)*nstep_cycle
+            for Mai in [0.65, 0.7, 0.75, 0.81]:
+                # Number of rotor blade passing periods to run for
+                # Change me so that the computaion reaches a periodic state
+                ncycle = 70
 
-        # Number of time steps between probes
-        nstep_save_probe = 1
+                # Time steps per cycle 
+                # Increased a bit because usually have fewer rotor blades
+                nstep_cycle = 96
 
-        # File name of the steady soln to read in
-        fname_now = fnamein + '_Ma_%.2f.hdf5' % Mai
+                # Which time step to start saving probes
+                nstep_save_start =  (ncycle-5)*nstep_cycle
 
-        # File name of the new unsteady input file to write out
-        fname_out = "input_2_Ma_%.2f.hdf5" % Mai
-        
-        # Put blade-to-blade probes on vane?
-        vane_b2b_probe = True
+                # Number of time steps between probes
+                nstep_save_probe = 1
 
-        #
-        # Should not need to change below this line
-        # It is complicated and not neccesary to understand the below!
-        #
+                # File name of the steady soln to read in fnamein + '_psi_%.2f' %Psii + '_phi_%.2f' %Phii + '_Ma_%.2f' % Mai + '.hdf5'
+                fname_now = fnamein + '_psi_%.2f' %Psii + '_phi_%.2f' %Phii + '_Ma_%.2f' % Mai + '.hdf5'
 
-        # Read in the converged steady solution
-        tsr = ts_tstream_reader.TstreamReader()
-        g = tsr.read(fname)
-        bids = g.get_block_ids()
+                # File name of the new unsteady input file to write out 'input_2_psi_%.2f' %Psii + '_phi_%.2f' %Phii + '_Ma_%.2f' % Mai + '.hdf5'
+                fname_out = 'input_2_psi_%.2f' %Psii + '_phi_%.2f' %Phii + '_Ma_%.2f' % Mai + '.hdf5'
+                
+                # Put blade-to-blade probes on vane?
+                vane_b2b_probe = True
 
-        # add blade-to-blade probe patches
-        if vane_b2b_probe:
-            bid_b2b = [0, 1]
-        else:
-            bid_b2b = [1]
+                #
+                # Should not need to change below this line
+                # It is complicated and not neccesary to understand the below!
+                #
 
-        for bid in bid_b2b:
-            b = g.get_block(bid)
-            jmid = int(b.nj/2)
-            p = ts_tstream_type.TstreamPatch()
-            p.kind = ts_tstream_patch_kind.probe
-            p.bid = bid
-            p.ist = 0
-            p.ien = b.ni
-            p.jst = jmid
-            p.jen = jmid+1
-            p.kst = 0
-            p.ken = b.nk
-            p.nxbid = 0
-            p.nxpid = 0
-            p.idir = 0
-            p.jdir = 1
-            p.kdir = 2
-            p.pid = g.add_patch(bid, p)
-            g.set_pv('probe_append', ts_tstream_type.int, p.bid, p.pid, 1)
+                # Read in the converged steady solution
+                tsr = ts_tstream_reader.TstreamReader()
+                g = tsr.read(fname)
+                bids = g.get_block_ids()
 
-        # find the sector size required
-        nb = np.array([g.get_bv('nblade',bid) for bid in bids])
-        dt = 2. * np.pi / nb
+                # add blade-to-blade probe patches
+                if vane_b2b_probe:
+                    bid_b2b = [0, 1]
+                else:
+                    bid_b2b = [1]
 
-        # loop over possible sector sizes
-        cost = np.empty((nb.max(),))
-        for i in range(nb.max()):
+                for bid in bid_b2b:
+                    b = g.get_block(bid)
+                    jmid = int(b.nj/2)
+                    p = ts_tstream_type.TstreamPatch()
+                    p.kind = ts_tstream_patch_kind.probe
+                    p.bid = bid
+                    p.ist = 0
+                    p.ien = b.ni
+                    p.jst = jmid
+                    p.jen = jmid+1
+                    p.kst = 0
+                    p.ken = b.nk
+                    p.nxbid = 0
+                    p.nxpid = 0
+                    p.idir = 0
+                    p.jdir = 1
+                    p.kdir = 2
+                    p.pid = g.add_patch(bid, p)
+                    g.set_pv('probe_append', ts_tstream_type.int, p.bid, p.pid, 1)
 
-            # Get scaling factor needed to fit the blades in this sector
-            dt_sector = 2. * np.pi / float(i+1)
-            nb_sector = np.round(dt_sector / dt)
-            sf_sector = dt_sector / nb_sector / dt
+                # find the sector size required
+                nb = np.array([g.get_bv('nblade',bid) for bid in bids])
+                dt = 2. * np.pi / nb
 
-            cost[i] = np.sum(np.abs(sf_sector - 1.))
+                # loop over possible sector sizes
+                cost = np.empty((nb.max(),))
+                for i in range(nb.max()):
 
-        # pick smallest sector with acceptable cost
-        thresh = 0.1
-        sect = (np.nonzero(cost<thresh))[-1][-1]
-        dt_sect = 2. * np.pi / sect
+                    # Get scaling factor needed to fit the blades in this sector
+                    dt_sector = 2. * np.pi / float(i+1)
+                    nb_sector = np.round(dt_sector / dt)
+                    sf_sector = dt_sector / nb_sector / dt
+
+                    cost[i] = np.sum(np.abs(sf_sector - 1.))
+
+                # pick smallest sector with acceptable cost
+                thresh = 0.1
+                sect = (np.nonzero(cost<thresh))[-1][-1]
+                dt_sect = 2. * np.pi / sect
 
 
-        # get blade numbers
-        dup = np.round(dt_sect/ dt)
-        scale = dt_sect / dup / dt
-        print 'Old blade counts', nb
-        print 'Sector size', sect, 'th annulus'
-        print 'Scaled blade counts', dup
-        print 'Scaling factors', scale
+                # get blade numbers
+                dup = np.round(dt_sect/ dt)
+                scale = dt_sect / dup / dt
+                print 'Old blade counts', nb
+                print 'Sector size', sect, 'th annulus'
+                print 'Scaled blade counts', dup
+                print 'Scaling factors', scale
 
-        # set frequency 
-        rpm = g.get_bv('rpm',bids[-1])
-        freq = rpm / 60. * (sect * dup)[0]
-        print 'frequency', freq, 'Hz'
+                # set frequency 
+                rpm = g.get_bv('rpm',bids[-1])
+                freq = rpm / 60. * (sect * dup)[0]
+                print 'frequency', freq, 'Hz'
 
-        # perodic patches
-        periodic = {}
-        periodic[0] = (0,0)
-        periodic[1] = (0,2)
-        periodic[2] = (1,0)
-        periodic[3] = (1,2)
-        print(periodic)
+                # perodic patches
+                periodic = {}
+                periodic[0] = (0,0)
+                periodic[1] = (0,2)
+                periodic[2] = (1,0)
+                periodic[3] = (1,2)
+                print(periodic)
 
-        dup_int = [int(dupi) for dupi in dup]
-        g2 = ts_tstream_steady_to_unsteady.steady_to_unsteady(
-                g, dup_int, scale, periodic
-                )
+                dup_int = [int(dupi) for dupi in dup]
+                g2 = ts_tstream_steady_to_unsteady.steady_to_unsteady(
+                        g, dup_int, scale, periodic
+                        )
 
-        # Get blade indices from circumferential periodics
-        p1 = g.get_patch(1,0)
-        p2 = g.get_patch(1,2)
-        ist = p1.ien
-        ien = p2.ist
+                # Get blade indices from circumferential periodics
+                p1 = g.get_patch(1,0)
+                p2 = g.get_patch(1,2)
+                ist = p1.ien
+                ien = p2.ist
 
-        # add blade probe patches
-        bid_pr = int(dup_int[0])
-        b = g2.get_block(bid_pr)
-        dk_free = 12
-        for k in [0, b.nk-1, dk_free, b.nk-1-dk_free]:
-            p = ts_tstream_type.TstreamPatch()
-            p.kind = ts_tstream_patch_kind.probe
-            p.bid = bid_pr
-            p.ist = ist
-            p.ien = ien
-            p.jst = 0
-            p.jen = b.nj
-            p.kst = k
-            p.ken = k+1
-            p.nxbid = 0
-            p.nxpid = 0
-            p.idir = 0
-            p.jdir = 1
-            p.kdir = 2
-            p.pid = g2.add_patch(bid_pr, p)
-            g.set_pv('probe_append', ts_tstream_type.int, p.bid, p.pid, 1)
+                # add blade probe patches
+                bid_pr = int(dup_int[0])
+                b = g2.get_block(bid_pr)
+                dk_free = 12
+                for k in [0, b.nk-1, dk_free, b.nk-1-dk_free]:
+                    p = ts_tstream_type.TstreamPatch()
+                    p.kind = ts_tstream_patch_kind.probe
+                    p.bid = bid_pr
+                    p.ist = ist
+                    p.ien = ien
+                    p.jst = 0
+                    p.jen = b.nj
+                    p.kst = k
+                    p.ken = k+1
+                    p.nxbid = 0
+                    p.nxpid = 0
+                    p.idir = 0
+                    p.jdir = 1
+                    p.kdir = 2
+                    p.pid = g2.add_patch(bid_pr, p)
+                    g.set_pv('probe_append', ts_tstream_type.int, p.bid, p.pid, 1)
 
-        # variables for unsteady run
-        g2.set_av("ncycle", ts_tstream_type.int, ncycle)
-        g2.set_av("frequency", ts_tstream_type.float,  freq)
+                # variables for unsteady run
+                g2.set_av("ncycle", ts_tstream_type.int, ncycle)
+                g2.set_av("frequency", ts_tstream_type.float,  freq)
 
-        g2.set_av("nstep_cycle", ts_tstream_type.int, nstep_cycle)
-        g2.set_av("nstep_inner", ts_tstream_type.int, 200)
+                g2.set_av("nstep_cycle", ts_tstream_type.int, nstep_cycle)
+                g2.set_av("nstep_inner", ts_tstream_type.int, 200)
 
-        # disable saving of snapshots
-        g2.set_av("nstep_save", ts_tstream_type.int, 999999)
+                # disable saving of snapshots
+                g2.set_av("nstep_save", ts_tstream_type.int, 999999)
 
-        # Save probes for last few period
-        g2.set_av("nstep_save_start", ts_tstream_type.int, nstep_save_start)
-        g2.set_av("nstep_save_start_probe", ts_tstream_type.int, nstep_save_start)
+                # Save probes for last few period
+                g2.set_av("nstep_save_start", ts_tstream_type.int, nstep_save_start)
+                g2.set_av("nstep_save_start_probe", ts_tstream_type.int, nstep_save_start)
 
-        # save probes every  nth step, from the beginning
-        g2.set_av("nstep_save_probe", ts_tstream_type.int, nstep_save_probe)
+                # save probes every  nth step, from the beginning
+                g2.set_av("nstep_save_probe", ts_tstream_type.int, nstep_save_probe)
 
-        # other configuration variables
-        g2.set_av("dts_conv", ts_tstream_type.float, 0.0005)
-        g2.set_av("facsafe", ts_tstream_type.float, 0.2)
-        g2.set_av("dts", ts_tstream_type.int, 1)
+                # other configuration variables
+                g2.set_av("dts_conv", ts_tstream_type.float, 0.0005)
+                g2.set_av("facsafe", ts_tstream_type.float, 0.2)
+                g2.set_av("dts", ts_tstream_type.int, 1)
 
-        # g2.set_av("sfin",ts_tstream_type.float,0.5)
-        # g2.set_av("facsecin",ts_tstream_type.float,0.005)
-        g2.set_av("dampin",ts_tstream_type.float,10.0)
+                # g2.set_av("sfin",ts_tstream_type.float,0.5)
+                # g2.set_av("facsecin",ts_tstream_type.float,0.005)
+                g2.set_av("dampin",ts_tstream_type.float,10.0)
 
-        # use mixing lengths and flow guess from steady calculation
-        g2.set_av("restart", ts_tstream_type.int, 1)
-        g2.set_av("poisson_restart", ts_tstream_type.int, 1)
-        g2.set_av("poisson_nstep", ts_tstream_type.int, 0)
+                # use mixing lengths and flow guess from steady calculation
+                g2.set_av("restart", ts_tstream_type.int, 1)
+                g2.set_av("poisson_restart", ts_tstream_type.int, 1)
+                g2.set_av("poisson_nstep", ts_tstream_type.int, 0)
 
-        # load balance for 1 GPUs
-        ts_tstream_load_balance.load_balance(g2, 1)
+                # load balance for 1 GPUs
+                ts_tstream_load_balance.load_balance(g2, 1)
 
-        # Reset spurious application variable
-        g2.set_av("if_ale", ts_tstream_type.int, 0)
+                # Reset spurious application variable
+                g2.set_av("if_ale", ts_tstream_type.int, 0)
 
-        # write out unsteady input file
-        g2.write_hdf5(fname_out)
+                # write out unsteady input file
+                g2.write_hdf5(fname_out)
--- Source/make_design.py
+++ Source/make_design.py
@@ -4,19 +4,23 @@ Choose parameters for a turbine stage and generate a corresponding CFD model.
 import design
 
 fname = 'input_1'  # Desired name of the TS input hdf5
-phi = 0.8  # Flow coefficient (0.4 to 1.2)
-psi = 1.6  # Stage loading coefficient (0.8 to 2.4)
+# phi = 0.8  # Flow coefficient (0.4 to 1.2)
+# psi = 1.6  # Stage loading coefficient (0.8 to 2.4)
 Lam = 0.5  # Degree of reaction (0.4 to 0.6)
 # Ma = 0.9  # Vane exit Mach number (0.6 to 0.9)
 eta = 0.9  # Polytropic efficiency (leave this for now)
 gap_chord = 0.5  # Spacing between stator and rotor
 
 # Check that the code works for many Mach
-for Mai in [0.6, 0.65, 0.7, 0.75, 0.81, 0.85, 0.9]:
+for Psii in [1.6]:
 
-    # Create a file name for this Mach using % substitution
-    fname_now = fname + '_Ma_%.2f.hdf5' % Mai
+    for Phii in [0.4, 0.6, 0.8, 1.0, 1.2]:
 
-    # Call out to the design generation code
-    # It is complicated so best to think of it as a black box!
-    design.generate(fname_now, phi, psi, Lam, Mai, eta, gap_chord )
+        for Mai in [0.6, 0.65, 0.7, 0.75, 0.81, 0.85, 0.9]:
+
+            # Create a file name for this Mach using % substitution
+            fname_now = fname + '_psi_%.2f' %Psii + '_phi_%.2f' %Phii + '_Ma_%.2f' % Mai + '.hdf5'
+
+            # Call out to the design generation code
+            # It is complicated so best to think of it as a black box!
+            design.generate(fname_now, Phii, Psii, Lam, Mai, eta, gap_chord)
