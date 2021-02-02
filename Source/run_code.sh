5534d5e2727bc85e429a4f8c0a9ce8d5dfc07ad6
Thomas K <tk518@cam.ac.uk>
2021-02-02 19:22:51 +0000

    Fixed Else -- else

--- Source/run_code.sh
+++ Source/run_code.sh
@@ -2,7 +2,7 @@
 #Create the steady input
 python make_design.py
 
-'''
+
 # Run the steady code through Turbostream
 mpirun -npernode 1 -np 1 turbostream input_1_psi_1.60_phi_0.45_Ma_0.70_slip.hdf5 output_1_psi_1.60_phi_0.45_Ma_0.70_slip 1
 
@@ -27,4 +27,3 @@ mpirun -npernode 1 -np 1 turbostream input_2_psi_1.60_phi_0.80_Ma_0.70_slip.hdf5
 mpirun -npernode 1 -np 1 turbostream input_2_psi_1.60_phi_1.00_Ma_0.70_slip.hdf5 output_2_psi_1.60_phi_1.00_Ma_0.70_slip 1
 
 mpirun -npernode 1 -np 1 turbostream input_2_psi_1.60_phi_1.15_Ma_0.70_slip.hdf5 output_2_psi_1.60_phi_1.15_Ma_0.70_slip 1
-'''
