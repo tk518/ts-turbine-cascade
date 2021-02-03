b1e7d88e78e6a1a6bdbb6ba85ecb48a00bc9fcc5
Thomas K <tk518@cam.ac.uk>
2021-02-03 17:02:44 +0000

    hopefully working now

--- Source/run_code_test.sh
+++ Source/run_code_test.sh
@@ -4,23 +4,23 @@
 #Create the steady input
 
 # Run the steady code through Turbostream
-for i in 0.80, 1.20, 1.60, 2.00, 2.40
+for i in 0.80 1.20 1.60 2.00 2.40
 do 
-    for x in 0.40, 0.60, 0.80, 1.00, 1.20
+    for x in 0.40 0.60 0.80 1.00 1.20
     do
-        echo "mpirun -npernode 1 -np 1 turbostream input_1_psi_$i_phi_$x_Ma_0.70.hdf5 output_1_psi_$i_phi_$x_Ma_0.70 1"
-        mpirun -npernode 1 -np 1 turbostream input_1_psi_$i_phi_$x_Ma_0.70.hdf5 output_1_psi_$i_phi_$x_Ma_0.70 1
+        echo "mpirun -npernode 1 -np 1 turbostream input_1_psi_"$i"_phi_"$x"_Ma_0.70.hdf5 output_1_psi_"$i"_phi_"$x"_Ma_0.70 1"
+        mpirun -npernode 1 -np 1 turbostream input_1_psi_"$i"_phi_"$x"_Ma_0.70.hdf5 output_1_psi_"$i"_phi_"$x"_Ma_0.70 1
     done
 done
 # Convert steady output to unsteady input
 python convert_unsteady.py
 
 # Run the unsteady code through Turbostream
-for i in 0.80, 1.20, 1.60, 2.00, 2.40
+for i in 0.80 1.20 1.60 2.00 2.40
 do 
-    for x in 0.40, 0.60, 0.80, 1.00, 1.20
+    for x in 0.40 0.60 0.80 1.00 1.20
     do
-        mpirun -npernode 1 -np 1 turbostream input_2_psi_$i_phi_$x_Ma_0.70.hdf5 output_2_psi_$i_phi_$x_Ma_0.70 1
+        mpirun -npernode 1 -np 1 turbostream input_2_psi_"$i"_phi_"$x"_Ma_0.70.hdf5 output_2_psi_"$i"_phi_"$x"_Ma_0.70 1
     done
 done
 
